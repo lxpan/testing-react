a246376f9f1fb0b4beac9fdb0c3f2611ddf3376d
Luoxi Pan <l.panpax@gmail.com>
2023-02-23 17:25:15 +1100

    Add real and fake timers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-    const [counter, setCounter] = useState(0);
-    return <button onClick={() => setCounter((x) => x + 1)}>{counter}</button>;
+    const [ctr, setCtr] = useState(0);
+    useEffect(() => {
+        setTimeout(() => setCtr(1), 1000);
+    }, []);
+    return ctr;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,17 +4,35 @@ import { act } from 'react-dom/test-utils';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-it('should increment a counter', () => {
+// it('should tick to a new value', () => {
+//     jest.useFakeTimers();
+
+//     render(<App />);
+
+//     const div = document.querySelector('div');
+//     expect(div.innerHTML).toBe('0');
+
+//     act(() => {
+//         jest.runAllTimers();
+//     });
+
+//     expect(div.innerHTML).toBe('1');
+// });
+
+it('should tick to a new value - real timers', async () => {
+    // a helper to use promises with timeouts
+    function sleep(period) {
+        return new Promise((resolve) => setTimeout(resolve, period));
+    }
+
     render(<App />);
-    // we attach the element to document.body to ensure events work
 
-    const button = screen.getByRole('button');
+    const div = document.querySelector('div');
+    expect(div.innerHTML).toBe('0');
 
-    act(() => {
-        for (let i = 0; i < 3; i++) {
-            userEvent.click(button);
-        }
+    await act(async () => {
+        await sleep(1100);
     });
 
-    expect(button.innerHTML).toBe('3');
+    expect(div.innerHTML).toBe('1');
 });
