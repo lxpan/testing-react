fb8b116b59e56bce05e033c5a4a89b7d9c054b5a
Luoxi Pan <l.panpax@gmail.com>
2023-02-23 17:30:16 +1100

    Bring in async verion of 'fetch' in component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
     const [data, setData] = useState(null);
+    async function somethingAsync() {
+        // this time we use the await syntax
+        const response = await fetch('/some/url');
+        setData(response);
+    }
     useEffect(() => {
-        fetch('/some/url').then(setData);
+        somethingAsync();
     }, []);
     return data;
 }
-
 export default App;
