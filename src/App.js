80fc89748495e105c83907fad9479ec440b0c78d
Luoxi Pan <l.panpax@gmail.com>
2023-02-23 12:41:15 +1100

    Fix User.Event.click test to use act() in setState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+
 function App() {
-    return <div>My App</div>;
+    const [counter, setCounter] = React.useState(0);
+
+    const increment = () => {
+        act(() => {
+            setCounter((prevCounter) => prevCounter + 1);
+        });
+    };
+
+    const decrement = () => {
+        act(() => {
+            setCounter((prevCounter) => prevCounter - 1);
+        });
+    };
+
+    return (
+        <div>
+            <h2 data-testid="counter">{counter}</h2>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import UserEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing App Component', () => {
+    test('counter is incremented on increment button click', () => {
+        render(<App />);
+
+        const counter = screen.getByTestId('counter');
+        const incrementBtn = screen.getByText('Increment');
+
+        UserEvent.click(incrementBtn);
+        UserEvent.click(incrementBtn);
+
+        expect(counter.textContent).toEqual('2');
+    });
+
+    test('counter is decremented on decrement button click', () => {
+        render(<App />);
+
+        const counter = screen.getByTestId('counter');
+        const decrementBtn = screen.getByText('Decrement');
+
+        UserEvent.click(decrementBtn);
+        UserEvent.click(decrementBtn);
+
+        expect(counter.textContent).toEqual('-2');
+    });
 });
