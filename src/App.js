9e3e3e04670aef9546f6183e80ebd348b5bd65a4
Luoxi Pan <l.panpax@gmail.com>
2023-02-23 13:30:05 +1100

    Add callback testing example (not working)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+
+export function Input(props) {
+    const { handleChange, inputValue } = props;
+    return <input onChange={handleChange} value={inputValue} />;
+}
+
 function App() {
-    return <div>My App</div>;
+    const [inputValue, setInputValue] = React.useState('');
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    return (
+        <div>
+            <Input handleChange={handleChange} inputValue={inputValue} />
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { waitFor, screen, render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import UserEvent from '@testing-library/user-event';
 import App from './App';
+import Input from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('input value is updated correctly', async () => {
+    render(<App />);
+
+    const input = screen.getByRole('textbox');
+
+    await waitFor(() => UserEvent.type(input, 'Foobar Jones'));
+
+    expect(input.value).toBe('Foobar Jones');
+});
+
+test('call the callback every time input value is changed', async () => {
+    // mock the onChange callback function
+    const handleChange = jest.fn();
+
+    // render only Input component
+    render(<Input handleChange={handleChange} inputValue="" />);
+
+    const input = screen.getByRole('textbox');
+
+    await waitFor(() => UserEvent.type(input, 'React'));
+
+    await waitFor(() => expect(handleChange).toHaveBeenCalledTimes(5));
 });
